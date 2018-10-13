e9f679966db500faeaacdb51c0018e510ed5be3b
JakobOttar <jakob.ottar@gmail.com>
2018-10-13 10:19:14 -0600

    Added test code for loading dataset

---
+++ script_test.js
@@ -0,0 +1,6 @@
+let init = function() {
+  let t1 = d3.csv("data/beer_reviews-1.csv");
+  let t2 = d3.csv("data/beer_reviews-2.csv");
+  let t3 = d3.csv("data/beer_reviews-3.csv");
+  let t4 = d3.csv("data/beer_reviews-4.csv");
+} // it takes 5.5 sec to load all data.
