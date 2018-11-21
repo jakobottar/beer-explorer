6b71873b8ea75ffa55b0eba2fe33b1a696e9d0f3
Derek Hunter <derek.hunter@aggiemail.usu.edu>
2018-11-21 00:03:16 -0700

    Does some more wiring and generates a beer table in addition to the brewery table

--- script_test.js
+++ script_test.js
@@ -1,6 +1,9 @@
-let init = function() {
-  let t1 = d3.csv("data/beer_reviews-1.csv");
-  let t2 = d3.csv("data/beer_reviews-2.csv");
-  let t3 = d3.csv("data/beer_reviews-3.csv");
-  let t4 = d3.csv("data/beer_reviews-4.csv");
-} // it takes 5.5 sec to load all data.
+let init = async function() {
+  let breweryData = await d3.csv('data/byBrewery-Locations.csv');
+
+  breweryData = breweryData.filter(function(el) {
+    return el.lat != 'NA' && el.lng != 'NA';
+  });
+
+  BuildTable(breweryData);
+};
