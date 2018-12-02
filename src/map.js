7ebc76df5bd80b66644803f65f06bf6d0ad7eade
froggiejj <33588648+froggiejj@users.noreply.github.com>
2018-12-02 11:27:45 -0700

    Updated about page, minor css changes

--- src/map.js
+++ src/map.js
@@ -235,8 +235,7 @@ class Map {
         .attr('cx', xLoc[i])
         .attr('cy', this.height - 35)
         .attr('class', circ_class[i])
-        // .attr("style", "stroke-width: 1.5")
-        .attr('r', 5);
+        .attr("style", "stroke-width: 1; r: 5")
     }
 
     mapLegend // add some help text, to instruct the user
